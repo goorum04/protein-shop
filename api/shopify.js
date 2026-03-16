7831e9abd20aa1f91554d128d5f42cfdd18d3c72
Said <s91564774@gmail.com>
2026-03-16 17:30:57 +0100

    Update shopify.js

--- api/shopify.js
+++ api/shopify.js
@@ -1,25 +1,16 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
-  const SHOPIFY_DOMAIN = '0bb404-4.myshopify.com';
-  const STOREFRONT_TOKEN = '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';
-
   const body = await req.text();
-
-  const response = await fetch(
-    'https://' + SHOPIFY_DOMAIN + '/api/2024-10/graphql.json',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Shopify-Storefront-Private-Token': STOREFRONT_TOKEN
-      },
-      body: body
-    }
-  );
-
-  const data = await response.json();
-
+  const res = await fetch('https://0bb404-4.myshopify.com/api/2024-10/graphql.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Shopify-Storefront-Private-Token': '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'
+    },
+    body: body
+  });
+  const data = await res.json();
   return new Response(JSON.stringify(data), {
     headers: {
       'Content-Type': 'application/json',
